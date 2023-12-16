57f96daf8539a1969424cba88f4744ea9eabce99
Andrii-Stepan Pryimak <andrii-stepan.pryimak.stud@pw.edu.pl>
2023-12-16 23:15:24 +0100

    Implemented finding the winning cluster and blocking the window after the game ends

--- classes/checking_for_winner.py
+++ classes/checking_for_winner.py
@@ -1,8 +1,9 @@
 
 class Graph:
     def __init__(self, board, color):
-        self.board = [[1 if cell.occupated_by == 'Green' else 2 if cell.occupated_by == 'Blue' else 0 for cell in row] for row in board]
+        self.board = [[1 if cell.occupated_by == 'Yellow' else 2 if cell.occupated_by == 'Blue' else 0 for cell in row] for row in board]
         self.color = color
+        self.winner = False
         if color == 1:
             self.check_positions = [(0, j) for j in range(11)]
             self.end_positions = [(10, j) for j in range(11)]
@@ -11,6 +12,10 @@ class Graph:
             self.end_positions = [(i, 10) for i in range(11)]
 
         if self.is_winner(self.check_positions):
+            self.winner, path = True, [self.is_winner(self.check_positions)[1]]
+            print(path)
+            self.wining_cluster = self.is_winner(path, True)
+
             print(f'Winner found {self.color}')
 
     def get_neighbors(self, i, j, color, visited):
@@ -21,29 +26,31 @@ class Graph:
             if 0 <= n <= 10 and 0 <= m <= 10 and self.board[n][m] == color and (n, m) not in visited:
                 neighbors.append((n, m))
                 if (n, m) in self.end_positions:
-                    return True, neighbors
+                    return True, neighbors, (n, m)
 
-        return False, neighbors
+        return False, neighbors, None
 
-    def is_winner(self, check_positions):
+    def is_winner(self, check_positions, move_through_all=False):
         visited = []
         while check_positions:
             depth_checking_positions = []
 
             for i, j in check_positions:
+                if self.board[i][j] != self.color:
+                    continue
                 visited.append((i, j))
-                end, neighbors = self.get_neighbors(i, j, self.color, visited)
+                end, neighbors, path = self.get_neighbors(i, j, self.color, visited)
                 depth_checking_positions.extend(neighbors)
 
-                if end:
-                    return True
+                if end and not move_through_all:
+                    return True, path
 
             check_positions = depth_checking_positions
+        if move_through_all:
+            return visited
 
 
-
-
-def exemple():
+def example():
     array = [[1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
              [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -55,9 +62,9 @@ def exemple():
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-    graph = Graph(array)
-    print(graph.get_neighbors(0,1,1,[(0,2)]))
+    graph = Graph(array, 1)
+    print(graph.get_neighbors(0, 1, 1, [(0, 2)]))
 
 
 if __name__ == '__main__':
-    exemple()
+    example()
--- classes/game.py
+++ classes/game.py
@@ -2,7 +2,8 @@ import pygame
 from .start_menu import Menu
 from .board import Board
 from .checking_for_winner import Graph
-
+from itertools import product
+import time
 
 class Game:
     def __init__(self, win):
@@ -13,6 +14,7 @@ class Game:
         self.board = Board()
         self.move = True
         self.game_started = False
+        self.game_ended = False
 
     def start_menu(self):
         self.menu.draw_menu(self.win)
@@ -27,21 +29,31 @@ class Game:
             self.game_started = True
             return 0
 
-        if self.game_started:
-            for i in range(0, 11):
-                for j in range(0, 11):
-                    if self.board.hex_cells[i][j].clicked(pos):
-                        if self.move:
-                            self.board.hex_cells[i][j].color = (0, 255, 0)
-                            self.board.hex_cells[i][j].occupated_by = 'Green'
-                            graph = Graph(self.board.hex_cells, 1)
-                            self.board.hex_cells[i][j].draw(self.win)
-                            self.move = not self.move
-                        elif not self.move:
-                            self.board.hex_cells[i][j].color = (0, 0, 255)
-                            self.board.hex_cells[i][j].occupated_by = 'Blue'
-                            graph = Graph(self.board.hex_cells, 2)
-                            self.board.hex_cells[i][j].draw(self.win)
-                            self.move = not self.move
+        if self.game_started and not self.game_ended:
+            for i, j in product(range(0, 11), repeat=2):
+
+                if self.board.hex_cells[i][j].clicked(pos):
+                    cell = self.board.hex_cells[i][j]
+                    if self.move:
+                        cell.color = (255, 255, 0)
+                        cell.occupated_by = 'Yellow'
+                    elif not self.move:
+                        cell.color = (0, 0, 255)
+                        cell.occupated_by = 'Blue'
+                    graph = Graph(self.board.hex_cells, 1 if self.move else 2)
+                    cell.draw(self.win)
+                    self.move = not self.move
+                    if graph.winner:
+                        self.game_ended = True
+                        for k in range(0, 10):
+                            color = (255, 255, 255) if k % 2 == 0 else (255, 255, 0) if graph.color == 1 else (0, 0, 255)
+                            for i, j in graph.wining_cluster:
+                                cell = self.board.hex_cells[i][j]
+                                cell.color = color
+                                cell.draw(self.win)
+                            pygame.display.flip()
+                            time.sleep(0.25)
+
+                    break
 
             pygame.display.flip()
