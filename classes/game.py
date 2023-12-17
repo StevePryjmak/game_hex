e0d586f048936ab4de12731882092ba74594f23a
Andrii-Stepan Pryimak <andrii-stepan.pryimak.stud@pw.edu.pl>
2023-12-17 17:28:42 +0100

    Introduced the get_hex_cords function,no need to iterate through all hexes. Additionally, refactored the __init__ method for improved clarity and structure. In the game file, I removed unnecessary 'for' and 'if' statements that were complicating the code excessively, making it less readable

--- classes/game.py
+++ classes/game.py
@@ -1,9 +1,9 @@
 import pygame
-from .start_menu import Menu
-from .board import Board
-from .checking_for_winner import Graph
-from itertools import product
+from classes.start_menu import Menu
+from classes.board import Board
+from classes.checking_for_winner import Graph
 import time
+
 
 class Game:
     def __init__(self, win):
@@ -27,33 +27,35 @@ class Game:
             pygame.display.flip()
             self.start_game = False
             self.game_started = True
-            return 0
+            return
 
         if self.game_started and not self.game_ended:
-            for i, j in product(range(0, 11), repeat=2):
+            i, j = self.board.get_hex_cords(pos)
+            print(i, j)
+            if i == -1:
+                return
 
-                if self.board.hex_cells[i][j].clicked(pos):
-                    cell = self.board.hex_cells[i][j]
-                    if self.move:
-                        cell.color = (255, 255, 0)
-                        cell.occupated_by = 'Yellow'
-                    elif not self.move:
-                        cell.color = (0, 0, 255)
-                        cell.occupated_by = 'Blue'
-                    graph = Graph(self.board.hex_cells, 1 if self.move else 2)
-                    cell.draw(self.win)
-                    self.move = not self.move
-                    if graph.winner:
-                        self.game_ended = True
-                        for k in range(0, 10):
-                            color = (255, 255, 255) if k % 2 == 0 else (255, 255, 0) if graph.color == 1 else (0, 0, 255)
-                            for i, j in graph.wining_cluster:
-                                cell = self.board.hex_cells[i][j]
-                                cell.color = color
-                                cell.draw(self.win)
-                            pygame.display.flip()
-                            time.sleep(0.25)
+            cell = self.board.hex_cells[i][j]
+            if self.move:
+                cell.color = (255, 255, 0)
+                cell.occupated_by = 'Yellow'
+            elif not self.move:
+                cell.color = (0, 0, 255)
+                cell.occupated_by = 'Blue'
+            graph = Graph(self.board.hex_cells, 1 if self.move else 2)
+            cell.draw(self.win)
+            self.move = not self.move
+            if graph.winner:
+                self.game_ended = True
+                for k in range(0, 10):
+                    color = (0, 255, 0) if k % 2 == 0 else (255, 255, 0) if graph.color == 1 else (0, 0, 255)
+                    for i, j in graph.wining_cluster:
+                        cell = self.board.hex_cells[i][j]
+                        cell.color = color
+                        cell.draw(self.win)
+                    pygame.display.flip()
+                    time.sleep(0.35)
 
-                    break
 
-            pygame.display.flip()
+
+        pygame.display.flip()
