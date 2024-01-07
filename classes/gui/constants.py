0c7efc884c563041396dbd601c3629bacdaeadc9
Andrii-Stepan Pryimak <andrii-stepan.pryimak.stud@pw.edu.pl>
2024-01-07 21:37:33 +0100

    W pliku constants.py istnieje możliwość dopasowania wartości `WIDTH` i `HEIGHT`, co pozwala na zmianę rozmiarów okna gry. Dla celów testowych, zwłaszcza przy sprawdzaniu funkcjonalności sieciowej na lokalnym hoście, zaleca się podzielenie tych wartości przez 2. Dzięki temu można uruchomić dwie instancje gry jednocześnie, co ułatwi przetestowanie komunikacji sieciowej w środowisku lokalnym.

--- classes/gui/constants.py
+++ classes/gui/constants.py
@@ -2,7 +2,7 @@ import pygame
 # Window constants
 pygame.init()
 info = pygame.display.Info()
-WIDTH, HEIGHT = info.current_w/2, info.current_h/2
+WIDTH, HEIGHT = info.current_w, info.current_h
 BG_COLOR = (50, 50, 50)
 FPS = 20
 
